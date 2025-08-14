55ffea312a728468bed74080c5ea78f50aa03dfb
Khizra-shahzad <khizrashahzad22@gmail.com>
2025-08-14 18:33:06 +0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Process from 'process'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: Process.env.VITE_BASE_PATH || '/Contact-Form'
 })
